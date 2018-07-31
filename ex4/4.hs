a5805c07adcd6c0dae97417759741d76651eb414
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2018-07-31 22:39:31 +0300

    Add exercise 4 part 4

--- ex4/4.hs
+++ ex4/4.hs
@@ -1,12 +1,19 @@
 import Data.List
 
-make_sieve :: Int -> [Int]
-make_sieve n =
-    let max_i = ceiling ((sqrt((fromIntegral (n + 1)))) / 2) :: Int in
-    let max_j = (\i -> div (n - 1) (2 * i + 1)) in
-    map (\(i, j) -> 2 * i * j + i + j)
-    (filter (\(i, j) -> (i <= max_i) && (j <= max_j i))
-    [(i, j) | i <- [1..n], j <- [1..n]])
+isqrt = floor . sqrt . fromIntegral
 
-sieveSundaram :: Int -> [Int]
-sieveSundaram n = map ((+1) . (*2)) ([1..n] \\ (make_sieve n))
+make_sieve :: Integer -> [Integer]
+make_sieve n =
+    let max_i = ceiling ((sqrt((fromIntegral (2 * n + 1)) - 1)) / 2) in
+    map (\(i, j) -> 2 * i * j + i + j)
+    $ filter (\(i, j) -> j <= div (n - i) (2 * i + 1))
+    [(i, j) | i <- [1..max_i], j <- [1..div (n - 1) 3]]
+
+sieve_sundaram :: Integer -> [Integer]
+sieve_sundaram n = map ((+1) . (*2)) ([1..n] \\ (make_sieve n))
+
+isPrime k = null [ x | x <- [2..isqrt k], k `mod`x  == 0]
+sieve_sundaram_test :: Bool
+sieve_sundaram_test =
+    let n = 10000
+    in (filter isPrime [3..2 * n + 2]) == (sieve_sundaram n)
