c2ea006c29317163abaa88e957c4243e56cb5369
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2018-08-22 19:39:48 +0300

    week 5 ex 4

--- ex5/Calculator.hs
+++ ex5/Calculator.hs
@@ -14,7 +14,38 @@ class Expr a where
     add :: a -> a -> a
     mul :: a -> a -> a
 
+newtype MinMax = MinMax Integer deriving (Eq, Ord, Show)
+newtype Mod7   = Mod7   Integer deriving (Eq, Show)
+
 instance Expr ExprT where
     lit value = Lit value
     add lhs rhs = Add lhs rhs
     mul lhs rhs = Mul lhs rhs
+
+instance Expr Integer where
+    lit value = value
+    add lhs rhs = lhs + rhs
+    mul lhs rhs = lhs * rhs
+
+instance Expr Bool where
+    lit value = value > 0
+    add lhs rhs = lhs || rhs
+    mul lhs rhs = lhs && rhs
+
+instance Expr MinMax where
+    lit value = MinMax value
+    add lhs rhs = maximum [lhs, rhs]
+    mul lhs rhs = minimum [lhs, rhs]
+
+instance Expr Mod7 where
+    lit value = Mod7 (mod value 7)
+    add (Mod7 lhs) (Mod7 rhs) = Mod7 (mod (lhs + rhs) 7)
+    mul (Mod7 lhs) (Mod7 rhs) = Mod7 (mod (lhs * rhs) 7)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool    = testExp :: Maybe Bool
+testMinMax  = testExp :: Maybe MinMax
+testSat     = testExp :: Maybe Mod7
